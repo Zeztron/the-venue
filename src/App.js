a0cc4124582cb1c91455bd0c6ac10f6537c8fc28
Zeztron <pateltharsh@gmail.com>
2019-02-09 11:11:23 -0500

    added sliders

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./resources/styles.css";
 
 import Header from "./components/header_footer/Header";
 import Footer from "./components/header_footer/Footer";
+import Featued from "./components/featured";
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height: "1500px", background: "cornflowerblue"}}>
         <Header/>
+        <Featued/>
       </div>
     );
   }
