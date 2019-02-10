16515b711cdbb1d833f076c08cbd5a658d91a132
Zeztron <pateltharsh@gmail.com>
2019-02-10 11:46:37 -0500

    created footer

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Fade from 'react-reveal/Fade';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div>
-        footer
-      </div>
-    )
-  }
-}
+const Footer = () => {
+  return (
+    <footer className="bck_red">
+      <Fade delay={500}>
+        <div className="font_righteous footer_logo_venue">
+          The Venue
+        </div>
+        <div className="footer_copyright">
+          The Venue 2019. All Rights Reserved.
+        </div>
+      </Fade>
+    </footer>
+  );
+};
+
+export default Footer;
